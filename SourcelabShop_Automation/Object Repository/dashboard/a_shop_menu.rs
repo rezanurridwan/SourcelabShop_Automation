61da0a69230f1782315488f50c5abc485c0b5cb9
LENOVO28\User <jeypecker01@gmail.com>
2025-07-18 10:50:44 +0700

    add algo for close btn

---
+++ SourcelabShop_Automation/Object Repository/dashboard/a_shop_menu.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_shop_menu</name>
+   <tag></tag>
+   <elementGuidId>9a20e55b-b8f2-4c0a-b2e2-c53d869dc35f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@class='header__menu-item header__menu-item list-menu__item link link--text focus-inset']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ SourcelabShop_Automation/Object Repository/dashboard/btn_close_pop_up.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_close_pop_up</name>
+   <tag></tag>
+   <elementGuidId>5924905c-fa2a-42ce-8fa0-dda4dc762e13</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div/div[1]/div/div/a[@aria-label='Close widget']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
